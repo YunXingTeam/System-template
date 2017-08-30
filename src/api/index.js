d769c42e20eb0d122ac6443ed8b58eac379d1029
2ylllll <495141992@qq.com>
2017-08-30 16:55:45 +0800

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -59,49 +59,14 @@ export const request = (params, showLoding) => {
 }
 /*请求配置*/
 export const config = {
-  // site: 'http://192.168.1.131:8080/',
-  // site: 'http://couldserver.chinacloudapp.cn:1882/',
-  site: 'http://couldserver.chinacloudapp.cn:1882/',
-  // imgHost: 'http://couldserver.chinacloudapp.cn:1882/',
-  imgHost: 'http://appjmzserver.chinacloudapp.cn:8080/',
+  site: 'http://192.168.1.131:8080/',
+  imgHost: 'http://192.168.1.131:8080/',
   action: 'Jyz_Data_I/jyz_Action',
-  signKey: 'zyt999',
+  signKey: '',
   logintype: 0,
 }
 
 export const YWID = {
-  /*微信配置*/
-  weChat: '5003',
-  /*登录*/
-  signIn: '1003',
-  /*静默登录*/
-  autoSignIn: '1003',
-  /*注册*/
-  signUp: '1002',
-  /*找回密码*/
-  retrieve: '1032',
-  /*注册获取验证码*/
-  codeZC: '1001',
-  /*找回密码获取验证码*/
-  codePSW: '1031',
-  /*修改信息*/
-  edit: '1023',
-  /*概况*/
-  introduce: '3001',
-  /*概况统计图*/
-  introduceChart: '3002',
-  /*各科室收入列表*/
-  ksIncomeList: '4011',
-  /*室收入统计图*/
-  ksIncomeChart: '4041',
-  /*科室内个医生收入列表*/
-  docIncomeList: '4051',
-  /*科室内个医生收入列表*/
-  docIncomeChart: '4061',
-  /*反馈*/
-  feedback: '4098',
-  /*签到*/
-  QD: '9003',
 }
 
 export const keys = {
